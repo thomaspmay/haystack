df341f8ffea0f6ec1486129d7e78eb4ab1a98fae
teemay <tom.may.tpm@gmail.com>
2021-01-13 00:52:44 +0000

    removed old directories

--- back/src/models/interfaces/loginDTO.interface.ts
+++
@@ -1,4 +0,0 @@
-export class loginDTO {
-    readonly email: string;
-    readonly password: string;
-  }
--- back/src/models/interfaces/registerDTO.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface registerDTO {
-    readonly id: number;
-    readonly firstName: string;
-    readonly lastName: string;
-    readonly userName : string;
-    readonly email: string;
-    readonly password: string;
-}
